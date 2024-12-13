6decceea93ee04d5e32c750cad414d7437bab7db
ewurasi nyarkoa <anewurasi@gmail.com>
2024-12-13 10:40:59 +0000

    added the mondoose connect in the server file

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 const Job = require('./models/Job'); // Import your Job model
 const app = express();
 const cors = require('cors');
